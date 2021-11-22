1d493bf06a4db37887dbe894950c01178a6255ab
Jim Halfpenny <jim@source321.com>
2021-11-22 16:09:08 +0000

    Updated to deploy containerised operators and services using helm charts.

--- quickstart/stackable-quickstart.sh
+++ quickstart/stackable-quickstart.sh
@@ -9,9 +9,11 @@ ALLOWED_OPERATORS=(zookeeper kafka nifi spark hive trino opa regorule)
 
 # Do you want to use the dev or release repository?
 REPO_TYPE=dev
+HELM_REPO_URL="https://repo.stackable.tech/repository/helm-${REPO_TYPE}/"
+HELM_REPO_NAME="stackable-operators"
 
 # List of operators to install
-OPERATORS=(zookeeper kafka nifi spark hive trino opa regorule)
+OPERATORS=(zookeeper)
 
 if [ $UID != 0 ]
 then
@@ -33,28 +35,12 @@ function print_g {
   /usr/bin/echo -e "\e[0;32m${1}\e[m"
 }
 
-# This is a dirty hack to get two K8s nodes running on a single machine.
-# Use the shortname if hostname returns the FQDN or vice versa.
-if [ "$(hostname -s)" = "$(hostname -f)" ]; then
-  print_r "Shortname matches FQDN. Host must have a dot in its hostname."
-  exit 1
-fi
-
-if [ "$(hostname)" = "$(hostname -f)" ]; then
-  K8S_HOSTNAME=`/usr/bin/hostname -s`
-else
-  K8S_HOSTNAME=`/usr/bin/hostname -f`
-fi
-
-GPG_KEY_URL="https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xce45c7a0a3e41385acd4358916dd12f5c7a6d76a"
-
 function install_prereqs {
   . /etc/os-release
 
   if [ "$ID" = "centos" ] || [ "$ID" = "redhat" ]; then
     if [ "$VERSION_ID" = "8" ] || [ "$VERSION_ID" = "7" ]; then
       print_g "$ID $VERSION found"
-      REPO_URL="https://repo.stackable.tech/repository/rpm-${REPO_TYPE}/el${VERSION_ID}"
       INSTALLER=/usr/bin/yum
       install_prereqs_redhat
     else
@@ -64,7 +50,6 @@ function install_prereqs {
   elif [ "$ID" = "ubuntu" ]; then
     if [ "$VERSION_ID" = "20.04" ]; then
       print_g "$ID $VERSION_ID found"
-      REPO_URL="https://repo.stackable.tech/repository/deb-${REPO_TYPE}"
       INSTALLER=apt
       install_prereqs_ubuntu
     else
@@ -74,7 +59,6 @@ function install_prereqs {
   elif [ "$ID" = "debian" ]; then
     if [ "$VERSION_ID" = "10" ]; then
       print_g "$ID $VERSION_ID found"
-      REPO_URL="https://repo.stackable.tech/repository/deb-${REPO_TYPE}"
       INSTALLER=apt
       install_prereqs_debian
     else
@@ -88,55 +72,18 @@ function install_prereqs {
 }
 
 function install_prereqs_redhat {
-  print_g "Installing Stackable YUM repo"
-
-  if [ -z $REPO_URL ]; then
-    print_r "No YUM repo URL found, exiting."
-    exit 1
-  fi
-
-  # Download the Stackable GPG key used for package signing
+  print_g "Installing prerequisite OS packages"
   /usr/bin/yum -y install gnupg2 java-11-openjdk curl python
-  /usr/bin/curl -s "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xce45c7a0a3e41385acd4358916dd12f5c7a6d76a" > /etc/pki/rpm-gpg/RPM-GPG-KEY-stackable
-  /usr/bin/rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-stackable
-
-  # Create YUM repo configuration file
-  # TODO: Enable GPG checking on Stackable repo
-  /usr/bin/echo "[stackable]
-name=Stackable ${REPO_TYPE} repo
-baseurl=${REPO_URL}
-enabled=1
-gpgcheck=0" > /etc/yum.repos.d/stackable.repo
-  /usr/bin/yum clean all
 }
 
 function install_prereqs_debian {
-  print_g "Installing Stackable APT repo"
-
-  if [ -z $REPO_URL ]; then
-    print_r "No APT repo URL found, exiting."
-    exit 1
-  fi
-
+  print_g "Installing prerequisite OS packages"
   apt-get -y install gnupg openjdk-11-jdk curl python
-  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 16dd12f5c7a6d76a
-  echo "deb $REPO_URL buster main" > /etc/apt/sources.list.d/stackable.list
-  apt clean
-  apt update
 }
 
 function install_prereqs_ubuntu {
-  print_g "Installing Stackable APT repo"
-
-  if [ -z $REPO_URL ]; then
-    print_r "No APT repo URL found, exiting."
-    exit 1
-  fi
-
+  print_g "Installing prerequisite OS packages"
   apt-get -y install gnupg openjdk-11-jdk curl python
-  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 16dd12f5c7a6d76a
-  echo "deb $REPO_URL buster main" > /etc/apt/sources.list.d/stackable.list
-  apt update
 }
 
 function install_k8s {
@@ -158,15 +105,16 @@ function install_k9s {
   /usr/bin/rm $TMPFILE
 }
 
+function install_helm {
+  print_g "Installing Helm"
+  /usr/bin/curl -sfL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | /bin/bash -
+  /usr/local/bin/helm repo add ${HELM_REPO_NAME} ${HELM_REPO_URL}
+}
+
 function install_crds {
   # TODO: Install the CRDs based on the list of operators to install
   print_g "Installing Stackable CRDs"
   kubectl apply -f "$CRDDIR"
-}
-
-function install_stackable_k8s_repo {
-  print_g Installing Stackable package repo
-  kubectl apply -f "$CONFDIR/stackable-repo.yaml"
 }
 
 function check_operator_list {
@@ -181,30 +129,16 @@ function check_operator_list {
 
 function install_operator {
   OPERATOR=$1
-  PKG_NAME=stackable-${OPERATOR}-operator
+  PKG_NAME=${OPERATOR}-operator
   print_g "Installing Stackable operator for ${OPERATOR}"
-
-  $INSTALLER -y install ${PKG_NAME}
-
-  /usr/bin/systemctl enable ${PKG_NAME}
-  /usr/bin/systemctl start ${PKG_NAME}
+  /usr/local/bin/helm install "${PKG_NAME}" "${HELM_REPO_NAME}/$PKG_NAME"
 }
 
 function install_stackable_operators {
   print_g "Installing Stackable operators"
   for OPERATOR in "${OPERATORS[@]}"; do
     install_operator $OPERATOR
-  done 
-}
-
-function install_stackable_agent {
-  print_g "Installing Stackable agent"
-  ${INSTALLER} -y install stackable-agent
-  echo "--hostname=$K8S_HOSTNAME" > /etc/stackable/stackable-agent/agent.conf
-  systemctl enable stackable-agent
-  systemctl start stackable-agent
-  kubectl certificate approve ${K8S_HOSTNAME}-tls
-  kubectl get nodes
+  done
 }
 
 function deploy_service {
@@ -234,20 +168,11 @@ install_k8s
 # Install k9s
 install_k9s
 
-# Install the Stackable CRDs
-install_crds
+# Install Helm
+install_helm
 
 # Install the Stackable operators for the chosen components
 install_stackable_operators
-
-# Install the Stackable agent
-install_stackable_agent
-
-# Create the Spark log directory as this is not auto-created by the service
-mkdir -p /data/spark/logs
-
-# Install the Stackable Kubernetes repo
-install_stackable_k8s_repo
 
 # Deploy Stackable Components
 for OPERATOR in "${OPERATORS[@]}"; do
